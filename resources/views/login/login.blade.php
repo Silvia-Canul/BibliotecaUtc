048a7e1892825f1f4cc8c2402e2d83a201b3055d
SALAZAR2019 <fiftyfrancisco2000@gmail.com>
2021-06-03 21:49:01 -0500

    este

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -29,7 +29,6 @@
       <input type="password" id="password" class="fadeIn third" name="password" placeholder="Contraseña" Required>
       <input type="submit" class="fadeIn fourth" value="Iniciar sesion">
     </form>
-    
   </div>
 </div>
 
